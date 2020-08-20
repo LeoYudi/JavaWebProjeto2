df6ceb8742373437bf904c75b4cfe11a6434b922
Leonardo Yudi <lol_leonardohiguti@outlook.com>
2020-08-20 15:30:06 -0300

    sessão funcionando

--- java/com/example/spring/controllers/UserController.java
+++ java/com/example/spring/controllers/UserController.java
@@ -9,7 +9,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class UserController {
@@ -20,10 +22,27 @@ public class UserController {
   private UserRepository ur;
   
   @RequestMapping(value = "/", method = RequestMethod.GET)
-  public String home(Model model) {
-    User user = ur.findByEmail("lol_leonardohiguti@outlook.com").get(0);
-    model.addAttribute("user", user.getName());
-    return "home";
+  public String home(Model model, HttpServletRequest request) {
+    try {
+      long id = (long) request.getSession().getAttribute("id");
+      boolean session = (boolean) request.getSession().getAttribute("session");
+      
+      Optional<User> userOp = ur.findById(id);
+      
+      if (userOp.isEmpty()) {
+        request.getSession().invalidate();
+        return "redirect:/login";
+      }
+      System.out.println(session);
+      if (!session)
+        request.getSession().invalidate();
+      
+      model.addAttribute("user", userOp.get().getName());
+      return "home";
+      
+    } catch (Exception e) {
+      return "redirect:/login";
+    }
   }
   
   @RequestMapping(value = "/register", method = RequestMethod.GET)
@@ -32,12 +51,15 @@ public class UserController {
   }
   
   @RequestMapping(value = "/register", method = RequestMethod.POST)
-  public String register(User user) {
+  public String register(User user, HttpServletRequest request) {
     user.setPassword(encoder.encode(user.getPassword()));
     List<User> users = ur.findByEmail(user.getEmail());
+    
     if (users.size() > 0)
-      return "redirect:/";
+      return "redirect:/register";
+    
     ur.save(user);
+    request.getSession().setAttribute("id", user.getId());
     return "redirect:/";
   }
   
@@ -47,15 +69,17 @@ public class UserController {
   }
   
   @RequestMapping(value = "/login", method = RequestMethod.POST)
-  public String login(String email, String password, boolean session, User user) {
+  public String login(String email, String password, boolean session, HttpServletRequest request) {
     String hash = encoder.encode(password);
-    user = ur.findByEmail(email).get(0);
+    User user = ur.findByEmail(email).get(0);
     if (user == null)
       return "redirect:/login";
     
     if (!encoder.matches(password, hash))
       return "redirect:/login";
     
+    request.getSession().setAttribute("id", user.getId());
+    request.getSession().setAttribute("session", session);
     
     return "redirect:/";
   }
